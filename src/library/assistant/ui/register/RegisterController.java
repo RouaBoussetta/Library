7bdbf759c4d530262aeca89373286d9ab721360c
Roua_Boussetta <roua.boussetta@esprit.tn>
2020-11-12 11:51:25 +0100

    commit

--- src/library/assistant/ui/register/RegisterController.java
+++ src/library/assistant/ui/register/RegisterController.java
@@ -7,12 +7,23 @@ package library.assistant.ui.register;
 
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import library.assistant.ui.login.LoginController;
+import library.assistant.util.LibraryAssistantUtil;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * FXML Controller class
@@ -20,6 +31,8 @@ import javafx.scene.control.Label;
  * @author Lenovo
  */
 public class RegisterController implements Initializable {
+        private final static Logger LOGGER = LogManager.getLogger(LoginController.class.getName());
+
     @FXML
     private JFXTextField username;
     @FXML
@@ -49,9 +62,27 @@ public class RegisterController implements Initializable {
     @FXML
     private void handleRegisterButtonAction(ActionEvent event) {
     }
-
+    private void closeStage() {
+        ((Stage) username.getScene().getWindow()).close();
+    }
     @FXML
     private void handleCancelButtonAction(ActionEvent event) {
+           closeStage();
+                    loadLogin();
+    }
+    
+     void loadLogin() {
+        try {
+           
+            Parent parent = FXMLLoader.load(getClass().getResource("/library/assistant/ui/login/login.fxml"));
+            Stage stage = new Stage(StageStyle.DECORATED);
+            stage.setTitle("Library Assistant Login");
+            stage.setScene(new Scene(parent));
+            stage.show();
+            LibraryAssistantUtil.setStageIcon(stage);
+        } catch (IOException ex) {
+            LOGGER.log(Level.ERROR, "{}", ex);
+        }
     }
     
 }
